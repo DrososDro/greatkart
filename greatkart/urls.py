f830012fe2568062732e9323f74813c908d2e555
Drosos <drosinakis.droso1@gmail.com>
2023-03-16 21:29:13 +0200

    continue with coursew

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from store import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("store/", include("store.urls")),
     path("cart/", include("carts.urls")),
+    path("", views.home, name="home"),
 ]
 urlpatterns += static(
     settings.MEDIA_URL,
--- store/urls.py
+++ store/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("store/", views.store, name="store"),
-    path("<slug:category_slug>/", views.store, name="products_by_category"),
+    path("", views.store, name="store"),
     path(
-        "<slug:category_slug>/<slug:product_slug>/",
+        "category/<slug:category_slug>/",
+        views.store,
+        name="products_by_category",
+    ),
+    path(
+        "category/<slug:category_slug>/<slug:product_slug>/",
         views.product_detail,
         name="products-details",
     ),
+    path("search/", views.search, name="search"),
 ]
